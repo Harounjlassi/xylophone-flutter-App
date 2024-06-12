df78b7cf14fbfe49321cf099a093e66e33c3eeec
Harounjlassi <jlassi.haroun@esprit.tn>
2024-06-12 14:04:08 +0100

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,114 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                AudioCache.instance = AudioCache(prefix: '');
-                final player = AudioPlayer();
-                await player.play(AssetSource('assets/audio/note1.wav'));
-              },
-              child: Text('TextButton'),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('assets/audio/note1.wav'));
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red, // Background color
+                  minimumSize: Size(100, 50), // Size of the button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, // Rectangle shape
+                  ),
+                ),
+                child: SizedBox.shrink(), // No child widget
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('assets/audio/note2.wav'));
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange, // Background color
+                  minimumSize: Size(100, 50), // Size of the button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, // Rectangle shape
+                  ),
+                ),
+                child: SizedBox.shrink(), // No child widget
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('assets/audio/note3.wav'));
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow, // Background color
+                  minimumSize: Size(100, 50), // Size of the button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, // Rectangle shape
+                  ),
+                ),
+                child: SizedBox.shrink(), // No child widget
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('assets/audio/note4.wav'));
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green, // Background color
+                  minimumSize: Size(100, 50), // Size of the button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, // Rectangle shape
+                  ),
+                ),
+                child: SizedBox.shrink(), // No child widget
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('assets/audio/note5.wav'));
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.teal, // Background color
+                  minimumSize: Size(100, 50), // Size of the button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, // Rectangle shape
+                  ),
+                ),
+                child: SizedBox.shrink(), // No child widget
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextButton(
+                onPressed: () async {
+                  AudioCache.instance = AudioCache(prefix: '');
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('assets/audio/note6.wav'));
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue, // Background color
+                  minimumSize: Size(100, 50), // Size of the button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, // Rectangle shape
+                  ),
+                ),
+                child: SizedBox.shrink(), // No child widget
+              ),
+            ],
           ),
         ),
       ),
