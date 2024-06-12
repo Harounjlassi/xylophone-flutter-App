11f240e5c69a10a58c75301e29b7186cfe16a16e
Harounjlassi <jlassi.haroun@esprit.tn>
2024-06-12 15:24:23 +0100

    add expanded wideget

--- lib/main.dart
+++ lib/main.dart
@@ -14,117 +14,115 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: [
-              TextButton(
-                onPressed: () {
-                  playSound(1);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.red, // Background color
-                  minimumSize: Size(100, 50), // Size of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero, // Rectangle shape
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red, // Background color
+                    minimumSize: Size(100, 50), // Size of the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // Rectangle shape
+                    ),
                   ),
+                  child: SizedBox.shrink(), // No child widget
                 ),
-                child: SizedBox.shrink(), // No child widget
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(2);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.orange, // Background color
-                  minimumSize: Size(100, 50), // Size of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero, // Rectangle shape
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange, // Background color
+                    minimumSize: Size(100, 50), // Size of the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // Rectangle shape
+                    ),
                   ),
+                  child: SizedBox.shrink(), // No child widget
                 ),
-                child: SizedBox.shrink(), // No child widget
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(3);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.yellow, // Background color
-                  minimumSize: Size(100, 50), // Size of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero, // Rectangle shape
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow, // Background color
+                    minimumSize: Size(100, 50), // Size of the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // Rectangle shape
+                    ),
                   ),
+                  child: SizedBox.shrink(), // No child widget
                 ),
-                child: SizedBox.shrink(), // No child widget
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(4);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green, // Background color
-                  minimumSize: Size(100, 50), // Size of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero, // Rectangle shape
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green, // Background color
+                    minimumSize: Size(100, 50), // Size of the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // Rectangle shape
+                    ),
                   ),
+                  child: SizedBox.shrink(), // No child widget
                 ),
-                child: SizedBox.shrink(), // No child widget
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(5);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.teal, // Background color
-                  minimumSize: Size(100, 50), // Size of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero, // Rectangle shape
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.teal, // Background color
+                    minimumSize: Size(100, 50), // Size of the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // Rectangle shape
+                    ),
                   ),
+                  child: SizedBox.shrink(), // No child widget
                 ),
-                child: SizedBox.shrink(), // No child widget
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(6);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue, // Background color
-                  minimumSize: Size(100, 50), // Size of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero, // Rectangle shape
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue, // Background color
+                    minimumSize: Size(100, 50), // Size of the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // Rectangle shape
+                    ),
                   ),
+                  child: SizedBox.shrink(), // No child widget
                 ),
-                child: SizedBox.shrink(), // No child widget
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextButton(
-                onPressed: () {
-                  playSound(7);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.deepPurpleAccent, // Background color
-                  minimumSize: Size(100, 50), // Size of the button
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero, // Rectangle shape
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        Colors.deepPurpleAccent, // Background color
+                    minimumSize: Size(100, 50), // Size of the button
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero, // Rectangle shape
+                    ),
                   ),
+                  child: SizedBox.shrink(), // No child widget
                 ),
-                child: SizedBox.shrink(), // No child widget
               ),
             ],
           ),
