810f046c2417b04350f171bfca0fe121efc3ff75
Harounjlassi <jlassi.haroun@esprit.tn>
2024-06-12 04:19:39 +0100

    ad audio and fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,16 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () async {
+                AudioCache.instance = AudioCache(prefix: '');
+                final player = AudioPlayer();
+                await player.play(AssetSource('assets/audio/note1.wav'));
+              },
+              child: Text('TextButton'),
+            ),
+          ),
         ),
       ),
     );
